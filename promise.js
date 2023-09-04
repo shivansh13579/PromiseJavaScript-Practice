31889d31716874144ab2d1f9345d13eb6c527887
shivansh13579 <shivansh0975@gmail.com>
2023-09-04 05:46:36 +0530

    async awit promise practice

--- promise.js
+++ promise.js
@@ -28,3 +28,45 @@ const promiseThree = new Promise(function(resolve,reject){
 promiseThree.then(function(user){
      console.log(user);
 })
+
+const promiseFour = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = false;
+        if(!error){
+            resolve({username: "shivam",Password : '123'})
+        }else{
+            reject('ERROR : Something went wrong')
+        }
+    },1000)
+})
+
+promiseFour.then((user)=>{
+      console.log(user);
+      return user.username;
+}).then((username)=>{
+       console.log(username);
+}).catch(function(error){
+     console.log(error);
+}).finally(()=>console.log("The Promise is either resolve or reject"))
+
+const promiseFive = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = true;
+        if(!error){
+            resolve({username:"JavaScript",Password:'123'})
+        }else{
+            reject("ERROR : JS went wrong")
+        }
+    },1000)
+});
+
+async function consumepromiseFive(){
+    try {
+    const response = await promiseFive
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+consumepromiseFive();
